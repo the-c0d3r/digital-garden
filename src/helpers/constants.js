ee220ac3a81355826ba4c02f752954bad5db6c16
the-c0d3r <4526565+the-c0d3r@users.noreply.github.com>
2026-09-01 14:55:15 +0800

    Update files

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,12 +1,13 @@
-exports.ALL_NOTE_SETTINGS= [
-    "dgHomeLink",
-    "dgPassFrontmatter",
-    "dgShowBacklinks",
-    "dgShowLocalGraph",
-    "dgShowInlineTitle",
-    "dgShowFileTree",
-    "dgEnableSearch",
-    "dgShowToc",
-    "dgLinkPreview",
-    "dgShowTags"
+// Core note settings. dgEnableSearch, dgLinkPreview, and dgShowFileTree
+// moved to the dg-search / dg-link-preview / dg-filetree plugins'
+// manifest "noteSettings".
+exports.ALL_NOTE_SETTINGS = [
+  "dgHomeLink",
+  "dgPassFrontmatter",
+  "dgShowBacklinks",
+  "dgShowLocalGraph",
+  "dgShowGraphDepthControl",
+  "dgShowInlineTitle",
+  "dgShowToc",
+  "dgShowTags",
 ];
